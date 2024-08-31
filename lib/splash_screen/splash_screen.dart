d55f9b0532f0085f7c3bac5f22412e60cb270540
iulia.filip <iulia.filip@newton.ro>
2024-09-01 00:25:16 +0300

    add ui for splash screen

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-
-import '../home_screen.dart';
+import 'package:gifthunterbetaversion/home_screen.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,7 +15,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 1, milliseconds: 500), () {
+    Timer(
+        const Duration(
+          seconds: 3,
+        ), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => const HomeScreen()),
       );
@@ -25,7 +28,24 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.yellow, child: const Center(child: Text('Splash Screen'))),
+      body: Container(
+        height: double.maxFinite,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/splash_screen_target.json', repeat: false),
+            const Text(
+              'G I F T H U N T',
+              style: TextStyle(
+                  fontSize: 32,
+                  fontFamily: 'SourceSans3',
+                  fontWeight: FontWeight.w800,
+                  color: Color(0xff241346)),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
